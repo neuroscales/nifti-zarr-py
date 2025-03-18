bb5ffcfe5766fd230bef127d0fa3cdf729dda0ff
balbasty <yael.balbastre@gmail.com>
2025-03-18 08:59:00 +0000

    MNT: expose __version__

--- niizarr/__init__.py
+++ niizarr/__init__.py
@@ -1,3 +1,11 @@
-from ._header import bin2nii
+try:
+    from ._version import __version__
+except (ImportError, ModuleNotFoundError):
+    # `pip install` has not been run.
+    # Unset __version__ rather than failing.
+    __version__ = None
+
+
+from ._header import bin2nii  # noqa: F401
 from ._nii2zarr import nii2zarr, nii2json  # noqa: F401
 from ._zarr2nii import zarr2nii  # noqa: F401
